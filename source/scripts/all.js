7ab874e4be631544c47c2e13d97a9204b2988bf0
Anthony D'Andrea <anthony.dandrea@me.com>
2014-11-22 22:21:09 -0600

    hero starting to look hero-y

--- source/scripts/all.js
+++ source/scripts/all.js
@@ -3,6 +3,18 @@ $(document).ready(function(){
     $(this).toggleClass('active');
     $('.mobile-nav ul').toggleClass('active');
   });
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
 
 // document.addEventListener("DOMContentLoaded", function() {
