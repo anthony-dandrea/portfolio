2cadf66fe3a51fd1cc4c2ea6799d3a68f190b3f8
Anthony D'Andrea <anthony.dandrea@me.com>
2014-11-23 15:09:42 -0600

    fix up the scroll for the header

--- source/scripts/all.js
+++ source/scripts/all.js
@@ -3,17 +3,18 @@ $(document).ready(function(){
     $(this).toggleClass('active');
     $('.mobile-nav ul').toggleClass('active');
   });
+
+  var headerHeight = $('header').height();
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
-        var headerHeight = $('header').height();
+        $('.menu, .mobile-nav ul').removeClass('active');
         target = target.offset().top - headerHeight;
         $('html,body').animate({
           scrollTop: target
         }, 1000);
-        $('.menu, .mobile-nav ul').removeClass('active');
         return false;
       }
     }
